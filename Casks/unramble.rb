f5ce3c6cf58d73b8a6349164c37698631c20f137
Mrinal Wadhwa <mrinal.wadhwa@gmail.com>
2026-07-22 23:08:07 -0700

    Update unramble to 0.2.0

--- Casks/unramble.rb
+++ Casks/unramble.rb
@@ -1,6 +1,6 @@
 cask "unramble" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.2.0"
+  sha256 "3489b7121fb1f73df6ba10e3fc3bcae5484f534e3b9695d9ebb86393acb05984"
 
   url "https://github.com/mrinalwadhwa/unramble/releases/download/v#{version}/Unramble.dmg"
   name "Unramble"
