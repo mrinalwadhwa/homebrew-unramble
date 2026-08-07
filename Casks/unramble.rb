664ff9ffc3ae0f58e6f0ce93bbc5c0e8635bc716
Mrinal Wadhwa <mrinal@ockam.io>
2026-08-07 03:11:18 -0700

    Update unramble to 0.5.0

--- Casks/unramble.rb
+++ Casks/unramble.rb
@@ -1,6 +1,6 @@
 cask "unramble" do
-  version "0.4.0"
-  sha256 "72a1b826e50ae8056a8e32cf3e78fd0561e8f81cd82631865aecd7554ca4198d"
+  version "0.5.0"
+  sha256 "5206f860412d40f5fd0b1882f2fd3168423eef74a07c295031382f508445010b"
 
   url "https://github.com/mrinalwadhwa/unramble/releases/download/v#{version}/Unramble.dmg"
   name "Unramble"
